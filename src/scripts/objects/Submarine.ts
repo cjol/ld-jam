23daba343842c16484b0f03229ec671d9c46a515
34will <wgilmour@hotmail.co.uk>
2021-04-24 10:53:39 +0100

    Modified .prettierrc to update styling rules. Modified files to update all stylings.

--- src/scripts/objects/Submarine.ts
+++ src/scripts/objects/Submarine.ts
@@ -1,47 +1,49 @@
 export default class Submarine extends Phaser.Physics.Arcade.Sprite {
-  // Constructor for submarine
-  constructor(scene, x, y) {
-    // Create submarine
-    super(scene, x, y, 'submarine')
-    scene.add.existing(this)
-    scene.physics.add.existing(this)
+    // Constructor for submarine
+    constructor(scene, x, y) {
+        // Create submarine
+        super(scene, x, y, 'submarine');
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
 
-    // Set physics params
-    this.body.setMass(100)
-    this.setDamping(true)
-    this.setDrag(0.25)
-    this.setMaxVelocity(200)
-    this.setCollideWorldBounds(false)
-  }
-
-  // Update loop - game physics tbc
-  update(cursors: Phaser.Types.Input.Keyboard.CursorKeys) {
-    // X direction - assume no key pressed
-    let somethingDownX = false
-    // Check for left and right keys
-    if (cursors.left.isDown) {
-      somethingDownX = true
-      this.setAccelerationX(-100)
-      this.setFlip(true, false)
-    } else if (cursors.right.isDown) {
-      somethingDownX = true
-      this.setAccelerationX(100)
-      this.setFlip(false, false)
+        // Set physics params
+        this.body.setMass(100);
+        this.setDamping(true);
+        this.setDrag(0.25);
+        this.setMaxVelocity(200);
+        this.setCollideWorldBounds(false);
     }
-    // If still no key pressed, set horizontal acceleration to 0
-    if (!somethingDownX) this.setAccelerationX(0)
 
-    // Y direction - assume no key pressed
-    let somethingDownY = false
-    // Check for up and down keys
-    if (cursors.up.isDown) {
-      somethingDownY = true
-      this.setAccelerationY(-100)
-    } else if (cursors.down.isDown) {
-      somethingDownY = true
-      this.setAccelerationY(100)
+    // Update loop - game physics tbc
+    update(cursors: Phaser.Types.Input.Keyboard.CursorKeys) {
+        // X direction - assume no key pressed
+        let somethingDownX = false;
+        // Check for left and right keys
+        if (cursors.left.isDown) {
+            somethingDownX = true;
+            this.setAccelerationX(-100);
+            this.setFlip(true, false);
+        } else if (cursors.right.isDown) {
+            somethingDownX = true;
+            this.setAccelerationX(100);
+            this.setFlip(false, false);
+        }
+        // If still no key pressed, set horizontal acceleration to 0
+        if (!somethingDownX)
+            this.setAccelerationX(0);
+
+        // Y direction - assume no key pressed
+        let somethingDownY = false;
+        // Check for up and down keys
+        if (cursors.up.isDown) {
+            somethingDownY = true;
+            this.setAccelerationY(-100);
+        } else if (cursors.down.isDown) {
+            somethingDownY = true;
+            this.setAccelerationY(100);
+        }
+        // If still no key pressed, set vertical acceleration to 0
+        if (!somethingDownY)
+            this.setAccelerationY(0);
     }
-    // If still no key pressed, set vertical acceleration to 0
-    if (!somethingDownY) this.setAccelerationY(0)
-  }
 }
--- src/scripts/scenes/mainScene.ts
+++ src/scripts/scenes/mainScene.ts
@@ -2,30 +2,30 @@ import Submarine from '../objects/Submarine'
 import FpsText from '../objects/fpsText'
 
 export default class MainScene extends Phaser.Scene {
-  fpsText: FpsText
-  cursor: Phaser.Types.Input.Keyboard.CursorKeys
-  submarine: Submarine
+    fpsText: FpsText;
+    cursor: Phaser.Types.Input.Keyboard.CursorKeys;
+    submarine: Submarine;
 
-  constructor() {
-    super({ key: 'MainScene' })
-  }
+    constructor() {
+        super({ key: 'MainScene' });
+    }
 
-  create() {
-    this.submarine = new Submarine(this, this.cameras.main.width / 2, 0)
-    this.fpsText = new FpsText(this)
+    create() {
+        this.submarine = new Submarine(this, this.cameras.main.width / 2, 0);
+        this.fpsText = new FpsText(this);
 
-    // display the Phaser.VERSION
-    this.add
-      .text(this.cameras.main.width - 15, 15, `Phaser v${Phaser.VERSION}`, {
-        color: '#000000',
-        fontSize: '24px'
-      })
-      .setOrigin(1, 0)
-    this.cursor = this.input.keyboard.createCursorKeys()
-  }
+        // display the Phaser.VERSION
+        this.add
+            .text(this.cameras.main.width - 15, 15, `Phaser v${Phaser.VERSION}`, {
+                color: '#000000',
+                fontSize: '24px'
+            })
+            .setOrigin(1, 0);
+        this.cursor = this.input.keyboard.createCursorKeys();
+    }
 
-  update() {
-    this.submarine.update(this.cursor)
-    this.fpsText.update()
-  }
+    update() {
+        this.submarine.update(this.cursor);
+        this.fpsText.update();
+    }
 }
