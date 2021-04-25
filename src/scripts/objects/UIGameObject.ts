02b289e13d580ebca6d83a8fb9d0eae9ba3f1248
Christopher Little <clittle@gsma.com>
2021-04-25 11:57:25 +0100

    More balanced hull

--- src/scripts/objects/UIGameObject.ts
+++ src/scripts/objects/UIGameObject.ts
@@ -92,8 +92,8 @@ export default class UIGameObject {
 
         // Show the pressure warning (overwrites low o2 if necessary)
         const {pressureWarning} = this.gameManager.submarine;
-        if (pressureWarning == 1) {this.warningMessage.setText('Hull Pressure Critical!').visible = true};    
-        if (pressureWarning == 2) {this.warningMessage.setText('Hull Failure').visible = true};        
+        if (pressureWarning == 1) {this.warningMessage.setText('Hull Failure').visible = true};        
+        if (pressureWarning == 2) {this.warningMessage.setText('Hull Pressure Critical!').visible = true};    
 
         // Show or don't show the upgrade menu
         if (!isAtSurface) {this.gameManager.upgradeMenuOpen = false};
