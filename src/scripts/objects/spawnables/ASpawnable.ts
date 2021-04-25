d149b74e6ab9fab06f88f8908dd28fa22f702cf4
34will <wgilmour@hotmail.co.uk>
2021-04-25 17:35:50 +0100

    Added Spawnable system.

---
+++ src/scripts/objects/spawnables/ASpawnable.ts
@@ -0,0 +1,78 @@
+import { ASpawnableBand } from "./ASpawnableBand";
+import { ISpawnableBandParameters } from "./ISpawnableBandParameters";
+import { ISpawnableParameters } from "./ISpawnableParameters";
+
+export abstract class ASpawnable<
+	T extends ISpawnableParameters,
+	K extends ISpawnableBandParameters
+> extends Phaser.Physics.Matter.Image {
+	protected readonly band: ASpawnableBand<T, K>;
+
+	protected started: boolean = false;
+	protected offscreen: boolean = false;
+
+	public dead: boolean = true;
+
+	public constructor(
+		scene: Phaser.Scene,
+		band: ASpawnableBand<T, K>,
+		baseTexture: string
+	) {
+		super(scene.matter.world, -400, -400, baseTexture, undefined, {
+			frictionAir: 0,
+			mass: 0.001
+		});
+		scene.add.existing(this);
+
+		this.band = band;
+
+		this.setSensor(true);
+		this.setIgnoreGravity(true);
+	}
+
+	public setParameters(parameters: T) {
+		this.x = parameters.position.x;
+		this.y = parameters.position.y;
+
+		this.displayWidth = this.width * parameters.scale;
+		this.displayHeight = this.height * parameters.scale;
+
+		this.setVelocity(
+			parameters.direction.x * parameters.speed,
+			parameters.direction.y * parameters.speed
+		);
+		this.setFlipX(true);
+		this.setFlipY(false);
+		this.setRotationDeg(parameters.directionAngle);
+
+		this.started = true;
+		this.offscreen = true;
+		this.dead = false;
+	}
+
+	public update() {
+		if (this.dead)
+			return;
+
+		const width = this.scene.cameras.main.width;
+		const bounds = this.getBounds();
+		if (this.started && this.offscreen) {
+			if (bounds.right <= width && bounds.left >= 0) {
+				this.started = false;
+				this.offscreen = false;
+			}
+		} else if (
+			!this.offscreen &&
+			(bounds.right < 0 || bounds.left > width)
+		) {
+			this.offscreen = true;
+			this.band.respawnItem(this);
+		}
+	}
+
+	protected setRotationDeg(angle: number): void {
+		if (angle > 165 && angle < 195)
+			this.setFlipY(true);
+		this.angle = angle;
+	}
+}
---
+++ src/scripts/objects/spawnables/ISpawnableParameters.ts
@@ -0,0 +1,8 @@
+export interface ISpawnableParameters {
+	position: Phaser.Math.Vector2;
+	directionAngle: number;
+	direction: Phaser.Math.Vector2;
+	speed: number;
+	scale: number;
+	type: number;
+}
