40d0bba2e92f4a91df0f337d26432a0a6f6b7a28
34will <wgilmour@hotmail.co.uk>
2021-04-25 21:48:25 +0100

    Modified MainScene to tidy it up, and fix the hazards not being updated.

--- src/scripts/scenes/MainScene.ts
+++ src/scripts/scenes/MainScene.ts
@@ -2,17 +2,13 @@ import Background from "../objects/Background";
 import { FishGroup } from "../objects/fish/FishGroup";
 import GameWorld from "../objects/GameWorld";
 import { HazardGroup } from "../objects/hazard/HazardGroup";
+import { ISpawnableGroup } from "../objects/spawnables/ISpawnableGroup";
 import Submarine from "../objects/Submarine";
 
 export default class MainScene extends Phaser.Scene {
-	private gameWorld: GameWorld;
 	private submarine: Submarine;
-	private fishGroup: FishGroup;
-	private hazardGroup: HazardGroup;
 	private background: Background;
-	private surfaceVessel: Phaser.GameObjects.Image;
-	private width: number;
-	private height: number;
+	private spawnableGroups: ISpawnableGroup[] = [];
 
 	constructor() {
 		super({ key: "MainScene" });
@@ -22,34 +18,37 @@ export default class MainScene extends Phaser.Scene {
 		this.cameras.main.fadeIn(500, 0, 0, 0);
 
 		// Get the game width and height
-		this.width = this.cameras.main.width;
-		this.height = this.cameras.main.height;
+		const width = this.cameras.main.width;
 
 		// Setup the 'world'
 		const maxDepth: number = 10000;
-		this.matter.world.setBounds(0, 0, this.width, maxDepth);
-		this.cameras.main.setBounds(0, 0, this.width, maxDepth);
+		this.matter.world.setBounds(0, 0, width, maxDepth);
+		this.cameras.main.setBounds(0, 0, width, maxDepth);
 
 		// Add some objects
 		this.background = new Background(this, maxDepth);
 
-		this.fishGroup = new FishGroup(this, this.background.SafeSpawnHeight);
-		this.hazardGroup = new HazardGroup(
-			this,
-			this.background.SafeSpawnHeight
+		this.spawnableGroups.push(
+			new FishGroup(this, this.background.SafeSpawnHeight)
 		);
-		this.submarine = new Submarine(this, this.width / 2);
-		this.gameWorld = new GameWorld(this, this.submarine);
+		this.spawnableGroups.push(
+			new HazardGroup(this, this.background.SafeSpawnHeight)
+		);
+		this.submarine = new Submarine(this, width / 2);
+
+		new GameWorld(this, this.submarine);
 
 		this.cameras.main.startFollow(this.submarine);
 		this.scene
 			.get("UIScene")
-			.events.on("upgraded", (e) => this.submarine.checkUpgrades());
+			.events.on("upgraded", () => this.submarine.checkUpgrades());
 	}
 
 	update(time: number, delta: number) {
 		this.background.draw();
 		this.submarine.update(time);
-		this.fishGroup.update(delta);
+
+		for (const spawnableGroup of this.spawnableGroups)
+			spawnableGroup.update(delta);
 	}
 }
